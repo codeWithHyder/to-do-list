7a4d409aa3e431683239a0abf6e653ff2806cd6c
Your Name <hyderali400@gmail.com>
2023-03-01 18:02:53 +0530

    js file updated and unused code lines removed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './style.css';
-/* import {addNewList,addToArray, displayTasks} from "./addtodo.js";
-import removeTask from "./removetask.js";
-addNewList();
-addToArray();
-displayTasks();
-removeTask(); */
 import toDoListDisplay from './basicstr.js';
 
 toDoListDisplay();
